8a4fa7636868177a7f407e2322702d6a8b4e2920
Steve Bang <mrsteve.bang@gmail.com>
2025-01-04 22:14:25 +0700

    change style for action play and pause, and use progress component from shacdn and add component Pomodoro and delete clock concept

--- src/components/SelectPomotimerType.tsx
+++ src/components/SelectPomotimerType.tsx
@@ -6,14 +6,14 @@ import { SelectItemProps } from "@/type";
 export interface SelectPomotimerTypeProps {
     onSelectTime: (value: string) => void;
     values: SelectItemProps[];
-    defaultValue: string;
+    defaultValue: number;
 }
 
 export default function SelectPomotimerType(
-    { onSelectTime, values, defaultValue } : SelectPomotimerTypeProps
+    { onSelectTime, values, defaultValue } : Readonly<SelectPomotimerTypeProps>
 ) {
-    console.log(defaultValue);
-    return (<Select onValueChange={(valueChange) => onSelectTime(valueChange)} defaultValue={defaultValue}>
+
+    return (<Select onValueChange={(valueChange) => onSelectTime(valueChange)} defaultValue={defaultValue.toString()}>
         <SelectTrigger className="w-[200px]">
             <SelectValue placeholder="Select time..."></SelectValue>
         </SelectTrigger>
